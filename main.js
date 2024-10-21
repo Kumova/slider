f017e55ae104d55dcf92fd7e63b43e1a9344ac0f
Daria <kumova_dasha@mail.ru>
2024-10-21 13:51:55 +0300

    внесла правки

--- main.js
+++ main.js
@@ -1,155 +1,177 @@
+
+const tabs = document.querySelectorAll('.about-navigation__item')
+const tabContents = document.querySelectorAll('.informations')
+const arrowTabs = document.querySelectorAll('.arrow')
+const imageSlide = document.querySelectorAll('img')
 const slider = document.querySelector('.slider');
 const sliderItems = Array.from(slider.children);
-const navigationSlider = document.querySelectorAll('.about-navigation__item');
-const rowRight = document.getElementById('right');
-const rowLeft = document.getElementById('left');
-const indicators = document.querySelectorAll('.arrow');
-const firstSlide = document.getElementById('1')
-const secondSlide = document.getElementById('2')
-const threeSlide = document.getElementById('3')
-const firstArrow = document.getElementById('once')
-const secondArrow = document.getElementById('two')
-const threeArrow = document.getElementById('three')
+const nextButtons = document.querySelectorAll('#right');
+const prevButtons = document.querySelectorAll('#left');
 
 
-
-const handeClickRight = (event) => {  
-  const currentSlide = slider.querySelector('[data-active]');
-  const currentSlideIndex = +currentSlide.dataset.index;
-  currentSlide.classList.add('hidden');
-  currentSlide.removeAttribute('data-active');
-  const nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1 ;
-  const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-  nextSlide.classList.remove('hidden');
-  nextSlide.setAttribute('data-active', '');
-};
-
-const handeClickLeft = (event) => {  
-  const currentSlide = slider.querySelector('[data-active]');
-  const currentSlideIndex = +currentSlide.dataset.index;
-  currentSlide.classList.add('hidden');
-  currentSlide.removeAttribute('data-active');
-  const nextSlideIndex = currentSlideIndex  === 0 ? sliderItems.length - 1 : currentSlideIndex - 1 ;
-  const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-  nextSlide.classList.remove('hidden');
-  nextSlide.setAttribute('data-active', '');
-};
-
-sliderItems.forEach(function (slide, index) {
-  if (index !==0) {
-    slide.classList.add('hidden');
-  }
-  slide.dataset.index = index;
-  sliderItems[0].setAttribute('data-active', '');
+tabs.forEach(tab => {
+  tab.addEventListener('click', function() {
+      const tabId = this.getAttribute('data-tab');
+      showTab(tabId);
+      
+  });
 });
 
-const showSlides = (event) => {
- 
-  const firstSlide = document.getElementsByTagName('li')[0];
-  const secondSlide = document.getElementsByTagName('li')[1];
-  const threeSlide = document.getElementsByTagName('li')[2];
-  const info = document.querySelectorAll('.informations');
-  const slide = document.querySelectorAll('img')
-  const arrow = document.querySelectorAll('.arrow')
-  slide[0].classList.add('hidden');
-  slide[1].classList.add('hidden');
-  slide[2].classList.add('hidden');
-  firstSlide.classList.remove('active')
-  secondSlide.classList.remove('active')
-  threeSlide.classList.remove('active')
-  info[1].classList.remove('active')
-  info[0].classList.remove('active')
-  info[2].classList.remove('active')
-  slide[0].removeAttribute('data-active');
-  slide[1].removeAttribute('data-active');
-  slide[2].removeAttribute('data-active');
-  arrow[0].classList.remove('active')
-  arrow[1].classList.remove('active')
-  arrow[2].classList.remove('active')
-
-  if (event.target.parentElement.id === '1') {
-    firstSlide.classList.add('active')
-    info[0].classList.add('active')
-    slide[0].classList.remove('hidden')
-    slide[0].setAttribute('data-active', '');
-    arrow[0].classList.add('active')
-  }
-
-  if (event.target.parentElement.id === '2') {
-    secondSlide.classList.add('active')
-    info[1].classList.add('active')
-    slide[1].classList.remove('hidden')
-    slide[1].setAttribute('data-active', '');
-    arrow[1].classList.add('active')
-  }
-
-  if (event.target.parentElement.id === '3') {
-    threeSlide.classList.add('active')
-    info[2].classList.add('active')
-    slide[2].classList.remove('hidden')
-    slide[2].setAttribute('data-active', '');
-    arrow[2].classList.add('active')
-  }
-};
+arrowTabs.forEach(arrow => {
+  arrow.addEventListener('click', function() {
+      const arrowId = this.getAttribute('data-tab-arrow');
+      showTab(arrowId);
+  });
+});
 
 
-const arrowSlides = (event) => {
-  const firstArrow = document.getElementById('once')
-  const secondArrow = document.getElementById('two')
-  const threeArrow = document.getElementById('three')
-  const info = document.querySelectorAll('.informations');
-  const slide = document.querySelectorAll('img')
-  const navigationSlider = document.querySelectorAll('.about-navigation__item');
-  firstArrow.classList.remove('active')
-  secondArrow.classList.remove('active')
-  threeArrow.classList.remove('active')
-  info[0].classList.remove('active')
-  info[1].classList.remove('active')
-  info[2].classList.remove('active')
-  navigationSlider[0].classList.remove('active');
-  navigationSlider[1].classList.remove('active');
-  navigationSlider[2].classList.remove('active');
-  slide[0].classList.add('hidden');
-  slide[1].classList.add('hidden');
-  slide[2].classList.add('hidden');
-  slide[0].removeAttribute('data-active');
-  slide[1].removeAttribute('data-active');
-  slide[2].removeAttribute('data-active');
+nextButtons.forEach(button => {
+  button.addEventListener('click', function() {
+      const slidesArrow = Array.from(this.parentElement.querySelectorAll('.arrow'));
+      let currentIndex = slidesArrow.findIndex(arrow => arrow.classList.contains('active'));
+      
+      if (currentIndex !== -1) {
+          slidesArrow[currentIndex].classList.remove('active');
 
-  if (event.target.id === 'once') {
-    console.log('дура')
-    firstArrow.classList.add('active')
-    info[0].classList.add('active')
-    navigationSlider[0].classList.add('active');
-    slide[0].classList.remove('hidden');
-    slide[0].setAttribute('data-active', '');
+          if (currentIndex + 1 < slidesArrow.length) {
+              currentIndex++;
+          } else {
+              currentIndex = 0; 
+          }
+
+          slidesArrow[currentIndex].classList.add('active');
+
+          switch (currentIndex) {
+              case 0:
+                  showTab('tab-1');
+                  break;
+              case 1:
+                  showTab('tab-2');
+                  break;
+              case 2:
+                  showTab('tab-3');
+                  break;
+          }
+      }
+  });
+});
+
+
+prevButtons.forEach(button => {
+  button.addEventListener('click', function() {
+      const slidesArrow = Array.from(this.parentElement.querySelectorAll('.arrow'));
+      let currentIndex = slidesArrow.findIndex(arrow => arrow.classList.contains('active'));
+      
+      if (currentIndex !== -1) {
+          slidesArrow[currentIndex].classList.remove('active');
+          currentIndex = (currentIndex - 1 + slidesArrow.length) % slidesArrow.length;
+          slidesArrow[currentIndex].classList.add('active');
+
+          switch (currentIndex) {
+              case 0:
+                  showTab('tab-1');
+                  break;
+              case 1:
+                  showTab('tab-2');
+                  break;
+              case 2:
+                  showTab('tab-3');
+                  break;
+          }
+      }
+  });
+});
+
+
+function showTab(tabId) {
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+
   
+  tabContents.forEach(content => {
+    content.classList.remove('active');
+  });
+
+  arrowTabs.forEach(arrow => {
+    arrow.classList.remove('active');
+  });
+
+  imageSlide.forEach(image => {
+    image.classList.add('hidden');
+  });
+
+  switch (tabId) {
+      case 'tab-1':
+          document.getElementById('tab-01').classList.add('active');
+          document.getElementById('tab-1').classList.add('active');
+          document.getElementById('tab1').classList.add('active');
+          document.getElementById('img1').classList.remove('hidden');
+          document.getElementById('img1').setAttribute('data-active', '');
+          break;
+      case 'tab-2':
+          document.getElementById('tab-02').classList.add('active');
+          document.getElementById('tab-2').classList.add('active');
+          document.getElementById('tab2').classList.add('active');
+          document.getElementById('img2').classList.remove('hidden');
+          document.getElementById('img2').setAttribute('data-active', '');
+          break;
+      case 'tab-3':
+          document.getElementById('tab-03').classList.add('active');
+          document.getElementById('tab-3').classList.add('active');
+          document.getElementById('tab3').classList.add('active');
+          document.getElementById('img3').classList.remove('hidden');
+          document.getElementById('img3').classList.add('active');
+          break;
   }
+}
+
+
+
+function showArrow(arrowId) {
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  tabContents.forEach(content => {
+    content.classList.remove('active');
+  });
+
+  arrowTabs.forEach(arrow => {
+    arrow.classList.remove('active');
+  });
+
+  imageSlide.forEach(image => {
+    image.classList.add('hidden');
+  });
+
   
-  if (event.target.id === 'two') {
-    secondArrow.classList.add('active')
-    info[1].classList.add('active')
-    navigationSlider[1].classList.add('active');
-    slide[1].classList.remove('hidden');
-    slide[1].setAttribute('data-active', '');
+  switch (arrowId) {
+      case 'tab1':
+          document.getElementById('tab-1').classList.add('active');
+          document.getElementById('tab1').classList.add('active');
+          document.getElementById('img1').classList.remove('hidden');
+          document.getElementById('img1').setAttribute('data-active', '');
+          break;
+      case 'tab2':
+          document.getElementById('tab-2').classList.add('active');
+          document.getElementById('tab2').classList.add('active');
+          document.getElementById('img2').classList.remove('hidden');
+          document.getElementById('img2').setAttribute('data-active', '');
+          break;
+      case 'tab3':
+          document.getElementById('tab-3').classList.add('active');
+          document.getElementById('tab3').classList.add('active');
+          document.getElementById('img3').classList.remove('hidden');
+          document.getElementById('img3').classList.add('active');
+          break;
   }
+}
 
-  if (event.target.id === 'three') {
-    threeArrow.classList.add('active')
-    info[2].classList.add('active')
-    navigationSlider[2].classList.add('active');
-    slide[2].classList.remove('hidden');
-    slide[2].setAttribute('data-active', '');
-  }
-};
+
+showTab('tab-1');
+showArrow('tab1');
 
 
 
-rowRight.addEventListener('click', handeClickRight);
-rowLeft.addEventListener('click', handeClickLeft);
-firstSlide.addEventListener('click', showSlides);
-secondSlide.addEventListener('click', showSlides);
-threeSlide.addEventListener('click', showSlides);
-firstArrow.addEventListener('click', arrowSlides);
-secondArrow.addEventListener('click', arrowSlides);
-threeArrow.addEventListener('click', arrowSlides);
+
